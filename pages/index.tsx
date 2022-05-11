6847995211041935b076c5e0a5f4fad45adc57df
KangSangHyeok <ksh44820@naver.com>
2022-05-11 16:37:35 +0900

    feat: svg 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
+import Radio from 'public/icon/radioOn.svg';
 
 const Home: NextPage = () => {
   return (
     <div>
       <div>
-        <h1 className='text-32 text-grey-10 '>Hello world!</h1>
-        <h1 className='text-32 text-grey-10 '>헬로우 월드</h1>
-        <h1 className='text-32 font-bold text-grey-10 '>Display 1</h1>
+        {/*<h1 className='text-32 text-grey-10 '>Hello world!</h1>*/}
+        {/*<h1 className='text-32 text-grey-10 '>헬로우 월드</h1>*/}
+        {/*<h1 className='text-32 font-bold text-grey-10 '>Display 1</h1>*/}
+        <Radio />
+        <Image src='/icon/radioOn.svg' width={24} height={24} />
       </div>
     </div>
   );
